8fc3a4e109075d575f53a25a0a58cb33108574d2
nur1002 <88443145+nur1002@users.noreply.github.com>
2022-11-18 15:06:07 +0300

    Add files via upload

---
+++ dongu3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class dongu3 {
+   public static void main(String[] args) {
+        int i=1,n,flag=0;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Siniri giriniz:");
+        n= inp.nextInt();
+        while(i<=n){
+            i*=4;
+
+            System.out.println(i);
+
+
+        }
+        while (i<=n){
+              i*=5;
+         System.out.println(i);
+}
+
+
+    }
+}
+
+
+
